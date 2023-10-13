69a93e5b4dc34d35734a3daa1104fb2b4cc5d91b
kungo-wu <1807973294@qq.com>
2023-10-13 21:29:35 +0800

    input system

--- Assets/Input System/PlayerMovement_1.cs
+++ Assets/Input System/PlayerMovement_1.cs
@@ -1,61 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 public class PlayerMovement_1 : MonoBehaviour
 {
-    private Rigidbody rb;
+    private CharacterController cr;
     private Animator anim;
 
     public float MoveSpeed;
-    private Vector3 MoveInput;
+    public PlayerInput inputControl;
+    public Vector3  MoveInput;
+    public Vector2  MoveInput0;
     private int combo = 0;
     private bool one = true;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        cr = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+        inputControl=new PlayerInput();
     }
-
-    private void Update()
+    private void OnEnable() 
+    {
+        inputControl.Enable();
+    }
+    private void OnDisable() 
+    {
+        inputControl.Disable();
+    }
+    private void Update() 
+     {
+        
+        Animate();
+        
+     }
+    private void FixedUpdate() 
     {
         Move();
-        Animate();
-        if (Input.GetMouseButtonDown(0))
-        {
-            anim.SetBool("isAttack", true);
-            if (combo >= 1)
-                anim.SetTrigger("attack1");
-            combo++;
-            Debug.Log("1");
-
-        }
     }
-
 
     private void Move()
     {
-        float horizontal1 = Input.GetAxisRaw("Horizontal");
-        float vertical1 = Input.GetAxisRaw("Vertical");
+        MoveInput0=inputControl.GamePlay.Move.ReadValue<Vector2>();
+        float horizontal1 = inputControl.GamePlay.Move.ReadValue<Vector2>().x;
+        float vertical1 = inputControl.GamePlay.Move.ReadValue<Vector2>().y;
 
         if (Mathf.Approximately(horizontal1, 0) && Mathf.Approximately(vertical1, 0))
         {
-            rb.velocity = Vector3.zero;
+            
             anim.SetBool("IsMoving", false); // No input, set IsMoving to false
             return;
         }
 
-        MoveInput = new Vector3(horizontal1, 0, vertical1).normalized;
-        rb.velocity = MoveInput * MoveSpeed * Time.fixedDeltaTime;
+       
+        cr.Move(new Vector3(MoveInput0.x*MoveSpeed*Time.deltaTime,-9.81f*Time.deltaTime,MoveInput0.y*MoveSpeed*Time.deltaTime));
         anim.SetBool("IsMoving", true); // There is input, set IsMoving to true
     }
 
     private void Animate()
     {
-        anim.SetFloat("MovementX", MoveInput.x);
-        anim.SetFloat("MovementZ", MoveInput.z);
+        anim.SetFloat("MovementX", MoveInput0.x);
+        anim.SetFloat("MovementZ", MoveInput0.y);
     }
 
 }
